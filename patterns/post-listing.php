dfb37efa8e2678c81ac12f3095054a7d8d3ffcf8
MichelleAaa <99776009+MichelleAaa@users.noreply.github.com>
2023-04-10 15:16:05 -0400

    CSS Updates and Blog Loop

---
+++ patterns/post-listing.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Title: Heading Slider
+ * Slug: the-office-supply-outlet/post-listing
+ * Categories: text
+ * inserter: no
+ */
+
+?>
+
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"","className":"index-query-loop-container"} -->
+<div class="wp-block-column index-query-loop-container"><!-- wp:query {"queryId":55,"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false}} -->
+<div class="wp-block-query"><!-- wp:post-template -->
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:post-featured-image /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:post-title {"isLink":true,"linkTarget":"_blank"} /-->
+
+<!-- wp:post-excerpt /-->
+
+<!-- wp:read-more /-->
+
+<!-- wp:post-author-name /-->
+
+<!-- wp:post-date /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+<!-- /wp:post-template -->
+
+<!-- wp:query-pagination -->
+<!-- wp:query-pagination-previous /-->
+
+<!-- wp:query-pagination-numbers /-->
+
+<!-- wp:query-pagination-next /-->
+<!-- /wp:query-pagination -->
+
+<!-- wp:query-no-results -->
+<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
+<p></p>
+<!-- /wp:paragraph -->
+<!-- /wp:query-no-results --></div>
+<!-- /wp:query --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
